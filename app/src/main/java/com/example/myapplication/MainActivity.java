58a8a0558fdd70a67a06a72e914b2abe4aa0e1eb
Diana <dianamonterrey04@gmail.com>
2026-03-31 21:05:19 -0600

    arreglos en el codig o

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,7 +2,7 @@ package com.example.myapplication;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View; // Importación necesaria para el OnClickListener
+import android.view.View;
 import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 
